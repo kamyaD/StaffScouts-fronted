a5434282516d6c171497011995fde84e19e7e853
Francis Masha <francismasha96@gmail.com>
2022-12-18 23:02:46 +0300

    chore: initiate project repository

---
+++ .github/readme.md
@@ -0,0 +1,5 @@
+<div align="center">
+
+## Staff Scouts
+
+</div>
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Staff Scouts</h3>
+    </div>
+  );
+}
+
+export default App;
